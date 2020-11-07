c3808765dacef38b6d4ffaf1c9c922b980795703
oyiuu <46087741+oyiuu@users.noreply.github.com>
2020-11-07 11:16:00 +0100

    fix

--- abot.py
+++ abot.py
@@ -7,33 +7,38 @@ bot = commands.Bot(
     command_prefix="?",
     help_command=None)
 
+
 @bot.event
 async def on_ready():
     print("I'm ready!")
     status = discord.Game("Prefix: ?")
     await bot.change_presence(activity=status)
 
+
 """@bot.event
 async def on_command_error(ctx, error):
     if isinstance(error, commands.CommandNotFound):
         await ctx.send("Command not found.")"""
 
+
 @bot.command()
 async def help(ctx):
-    embed=discord.Embed(title="Prefix: ,", description="A list of all available commands", color=discord.Color.purple)
+    embed = discord.Embed(title="Prefix: ?", description="A list of all available commands",
+                          color=discord.Color.purple())
     member = ctx.message.author
     embed.set_author(name="Help", icon_url="{}".format(member.avatar_url))
-    embed.add_field(name="Prune (num)", inline=False)
-    embed.add_field(name="Kick", inline=False)
-    embed.add_field(name="Ban", inline=False)
-    embed.add_field(name="Avatar (id/ping)", inline=False)
-    embed.add_field(name="Info", inline=False)
+    embed.add_field(name="Prune", inline=False, value='Deletes specified amount of messages.')
+    embed.add_field(name="Kick", inline=False, value='Removes a member from your server.')
+    embed.add_field(name="Ban", inline=False, value='Bans a member from your server.')
+    embed.add_field(name="Avatar (id/ping)", inline=False, value='Returns the avatar of the user.')
+    embed.add_field(name="Info", inline=False, value='Returns information about user.')
     embed.set_footer(text="More commands to come.")
     await ctx.send(embed=embed)
 
+
 @bot.command()
-async def info(ctx, member: discord.Member):
-    embed=discord.Embed(color=discord.Color.purple())
+async def info(ctx, member: discord.Member=None):
+    embed = discord.Embed(color=discord.Color.purple())
     if not member:
         member = ctx.message.author
     embed.set_author(name=f"{member}", icon_url=member.avatar_url)
@@ -47,11 +52,14 @@ async def info(ctx, member: discord.Member):
         embed.add_field(name="Roles", value=" | ".join(reversed(roles)), inline=False)
     await ctx.send(embed=embed)
 
+
 @bot.command()
 @has_permissions(kick_members=True)
-async def kick(ctx, member:discord.Member):
+async def kick(ctx, member: discord.Member):
     await ctx.send(f"{member} was kicked!")
     await member.kick()
+
+
 @kick.error
 async def kick_error(ctx, error):
     if isinstance(error, CheckFailure):
@@ -61,11 +69,14 @@ async def kick_error(ctx, error):
     else:
         await ctx.send("Invalid ID/tag")
 
+
 @bot.command()
-@has_permissions(ban_members=True)  
-async def ban(ctx, member:discord.Member): 
+@has_permissions(ban_members=True)
+async def ban(ctx, member: discord.Member):
     await ctx.send(f'{member} was banned!')
     await member.ban()
+
+
 @ban.error
 async def ban_error(ctx, error):
     if isinstance(error, CheckFailure):
@@ -75,20 +86,24 @@ async def ban_error(ctx, error):
     else:
         await ctx.send("Invalid ID/tag")
 
+
 @bot.command()
-async def avatar(ctx, member: discord.Member):
+async def avatar(ctx, member: discord.Member=None):
     if not member:
-        member = ctx.message.author
+        member = ctx.author
     show_avatar = discord.Embed(title=" ", color=discord.Color.purple())
     show_avatar.set_image(url=member.avatar_url)
     show_avatar.set_author(name=f"{member}")
     await ctx.send(embed=show_avatar)
 
+
 @bot.command()
 @has_permissions(manage_messages=True)
-async def prune(ctx, amount : int):
+async def prune(ctx, amount: int):
     await ctx.channel.purge(limit=amount)
     await ctx.send(f"Deleted {amount} message(s)!", delete_after=5)
+
+
 @prune.error
 async def prune_error(ctx, error):
     if isinstance(error, CheckFailure):
@@ -96,15 +111,16 @@ async def prune_error(ctx, error):
     elif isinstance(error, commands.MissingRequiredArgument):
         await ctx.send("Please specify an amount.")
 
-    
+
 @bot.event
-async def on_message(message):         
-    if message.author == bot.user: 
-        return 
-    message_lower = message.content.lower()    
-    if message_lower == "hello":   
+async def on_message(message):
+    if message.author == bot.user:
+        return
+    message_lower = message.content.lower()
+    if message_lower == "hello":
         await message.channel.send("Hi!")
 
-    await bot.process_commands(message)  #on_message blocks commands
+    await bot.process_commands(message)  # on_message blocks commands
 
-bot.run(config.TOKEN) 
+
+bot.run(config.TOKEN)
